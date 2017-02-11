bd19be608c80357202073865598fc142d45ad07f
Vu Phan <vuphan314@gmail.com>
2017-02-11 15:40:48 -0600

    c++ boilerplate

---
+++ src/src.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <gmp.h>
+
+#include "Cryptoframework.cpp"
+
+using namespace std;
+
+////////////////////////////////////////////////////////////
+
+int main() {
+  cout << "cry" << "\n";
+}
