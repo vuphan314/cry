c34242c1313e517a6b80b3e45197dd8e6ccaaf19
Vu Phan <vuphan314@gmail.com>
2017-03-25 13:01:41 -0500

    testTypes

--- project4part1/code/vu/tutorial.cc
+++ project4part1/code/vu/tutorial.cc
@@ -4,4 +4,17 @@
 
 void testTutorial() {
   cout << "Testing tutorial.\n";
+  testTypes();
+}
+
+void testTypes() {
+  cout << "Testing types.\n";
+  string s("1000000000000000000000000000000000000000000");
+
+  // long long ll = stoll(s);
+    // terminate called after throwing
+    // an instance of 'std::out_of_range'
+
+  mpz_class z(s);
+  cout << "Big int: " << z << ".\n";
 }
