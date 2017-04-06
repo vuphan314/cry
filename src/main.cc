300a17fd39f4879c5326a98e7c42510cf5b28d85
Vu Phan <vuphan314@gmail.com>
2017-04-05 23:04:36 -0500

    demoRsa

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,8 @@
 
 ////////////////////////////////////////////////////////////
 
-int main() {
+void testCryptosystems() {
+  std::cout << "\nfunction testCryptosystems:\n";
   std::vector<CryptosystemName>
     cryptosystemNames{RSA, DUMMY};
   for (CryptosystemName cryptosystemName :
@@ -15,4 +16,42 @@ int main() {
       std::cout << "Some test failed.\n";
     }
   }
+}
+
+////////////////////////////////////////////////////////////
+
+void demoRsa() {
+  std::cout << "\nfunction demoRsa:\n\n";
+  RsaCryptosystem rsaCryptosystem;
+
+  Key publicKey, privateKey;
+  rsaCryptosystem.generateKeys(publicKey, privateKey);
+  std::cout << "key-generation:\n\tpublickey:\n\t\t" <<
+    publicKey[0] << "\n\t\t" << publicKey[1] <<
+    "\n\tprivate key:\n\t\t" << privateKey[0] << "\n\t\t" <<
+    privateKey[1] << "\n";
+
+  Text plainText = "my string", cipherText;
+  std::cout << "plaintext: " << plainText << "\n";
+
+  rsaCryptosystem.encrypt(cipherText, plainText, publicKey);
+  std::cout << "encryption:\n\tciphertext: " <<
+    cipherText << "\n";
+
+  rsaCryptosystem.decrypt(plainText, cipherText,
+    privateKey);
+  std::cout << "decryption:\n\tplaintext: " << plainText <<
+    "\n";
+
+  rsaCryptosystem.cryptanalyze(plainText, cipherText,
+    publicKey);
+  std::cout << "cryptanalysis:\n\tplaintext: " <<
+    plainText << "\n";
+}
+
+////////////////////////////////////////////////////////////
+
+int main() {
+  // testCryptosystems();
+  demoRsa();
 }
