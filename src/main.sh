0e50cd657899f952b8dd4a4b0fef10f75c578f98
Vu Phan <vuphan314@gmail.com>
2017-04-12 23:39:20 -0500

    about to ditch padding ciphertext

--- src/main.sh
+++ src/main.sh
@@ -6,8 +6,8 @@ plainText=$2
 # minModulusLength=32 # 0s cryptanalysis
 # plainText=0123
 
-minModulusLength=46 # 8s cryptanalysis
-plainText=four6
+# minModulusLength=46 # 8s cryptanalysis
+# plainText=four6
 
 # minModulusLength=64 # 1h cryptanalysis
 # plainText=01234567
